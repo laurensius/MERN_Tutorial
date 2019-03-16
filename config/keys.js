e4d0be08e4622a25568ce14d7c49e46b6aaaa548
laurensius <burglar.tea@gmail.com>
2019-03-16 13:57:36 +0700

    Create Users Model

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
   mongooURI: "mongodb://root:laurens23@localhost:27017/admin"
+  //   mongooURI: "mongodb+srv://root:Laurens23!@cluster0-fwtny.mongodb.net/test?retryWrites=true"
 };
