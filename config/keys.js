be8535554bd1c2328c9509588b81dae5f5294675
laurensius <burglar.tea@gmail.com>
2019-03-16 16:29:20 +0700

    Registration Route, add dependency (gravatar)

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongooURI: "mongodb://root:laurens23@localhost:27017/admin"
+  mongooURI: "mongodb://renzcybermedia:laurens23@localhost:27017/devconnector"
   //   mongooURI: "mongodb+srv://root:Laurens23!@cluster0-fwtny.mongodb.net/test?retryWrites=true"
 };
