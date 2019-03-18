a824338210a397b96c898a33f3a7caed62e089be
laurensius <burglar.tea@gmail.com>
2019-03-18 12:20:03 +0700

    Create JWT (JSON Web Token)

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-  mongooURI: "mongodb://renzcybermedia:laurens23@localhost:27017/devconnector"
   //   mongooURI: "mongodb+srv://root:Laurens23!@cluster0-fwtny.mongodb.net/test?retryWrites=true"
+
+  mongooURI: "mongodb://renzcybermedia:laurens23@localhost:27017/devconnector",
+  secretOrKey: "mysecret"
 };
