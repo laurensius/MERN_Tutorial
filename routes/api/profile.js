69c89240ec20e793c931bc7cd47e9ef92f302635
laurensius <burglar.tea@gmail.com>
2019-03-19 22:08:35 +0700

    Current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
+const monggose = require("mongoose");
 const passport = require("passport");
 
 // load profile model
